f88d8e50a53bf8a4252b83e9704ee1d44c19d67e
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-16 11:54:09 +0100

    Exo5

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $gender='Femme';
+    if ($gender != 'Homme') {
+      echo 'C\'est une développeuse !!!!';
+    }
+    else {
+      echo 'C\'est un développeur !!!!';
+    }
+    ?>
+  </body>
